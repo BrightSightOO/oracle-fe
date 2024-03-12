d7155c759dbb8e69fd89c3720eaf65b11dfb5945
Arshak Asriyan <45574365+AAsriyan@users.noreply.github.com>
2024-03-12 15:44:40 -0500

    added navigation components

---
+++ src/components/Navigation/Header.tsx
@@ -0,0 +1,38 @@
+import { MainColorSet } from '@/theme/types';
+import { Box, Flex, HStack, Show, Text, useTheme } from '@chakra-ui/react';
+import Navigation from './Navigation';
+import dynamic from 'next/dynamic';
+
+const Header = () => {
+  const { colors } = useTheme();
+  const { white, black } = colors as MainColorSet;
+
+  const WalletMultiButtonDynamic = dynamic(
+    async () =>
+      (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+    { ssr: false }
+  );
+
+  return (
+    <Box w='full' bg={black}>
+      <HStack
+        justifyContent='space-between'
+        maxW='1144px'
+        w='full'
+        mx='auto'
+        h='66px'
+        px='16px'
+      >
+        <Text color={white}>Oracle</Text>
+        <Navigation />
+        <Show above='md'>
+          <Flex justifyContent='center' minW='190px'>
+            <WalletMultiButtonDynamic />
+          </Flex>
+        </Show>
+      </HStack>
+    </Box>
+  );
+};
+
+export default Header;
