038626ac416b0a78f534d7fa8f41621a7c15edac
Arshak Asriyan <45574365+AAsriyan@users.noreply.github.com>
2024-10-01 07:22:22 -0500

    remade home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,40 @@
-import { Flex, useTheme } from '@chakra-ui/react';
+import Requests from '@/components/Requests';
+import Stake from '@/components/Stake';
+import StakingBottomSheet from '@/components/Stake/StakingBottomSheet';
 import { MainColorSet } from '@/theme/types';
+import { Flex, HStack, Show, useDisclosure, useTheme } from '@chakra-ui/react';
 import Head from 'next/script';
-import Proposals from '@/components/Proposals/Proposals';
 
 export default function Home() {
-  const { colors } = useTheme();
-  const { backgroundMain } = colors as MainColorSet;
+	const { colors } = useTheme();
+	const { backgroundMain } = colors as MainColorSet;
 
-  return (
-    <Flex mx='auto' flexDir='column' w='100%' bg={backgroundMain}>
-      <Head id='Optimistic Oracle'>
-        <title>Optimistic Oracle</title>
-      </Head>
-      <Proposals />
-    </Flex>
-  );
+	const stakeModal = useDisclosure();
+
+	return (
+		<Flex w='full' mx='auto' px='16px' flexDir='column' bg={backgroundMain}>
+			<Head id='BrightSight'>
+				<title>BrightSight - Home</title>
+			</Head>
+			<HStack
+				w='full'
+				mx='auto'
+				maxW='1512px'
+				py='50px'
+				gap={10}
+				justifyContent='space-between'
+			>
+				<Requests stakingToggle={stakeModal.onToggle} />
+				<Show above='md'>
+					<Stake />
+				</Show>
+				<Show below='md'>
+					<StakingBottomSheet
+						isOpen={stakeModal.isOpen}
+						onClose={stakeModal.onClose}
+					/>
+				</Show>
+			</HStack>
+		</Flex>
+	);
 }
